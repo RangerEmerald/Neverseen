aed754193adc2403b34190ce796fcd3c724e7bb8
Ranger Emerald <58153205+RangerEmerald@users.noreply.github.com>
2021-03-09 12:54:39 -0700

    Fixed bug when querying and storing
    Fixed a big where it would concatenate two numbers like strings instead of adding them together like numbers

--- src/databases/SQLite/queries.js
+++ src/databases/SQLite/queries.js
@@ -3,9 +3,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 //VARIABLES
 let scoreMapCache = new Map();
-let db = new sqlite3.Database('src/databases/database.db');
+let db = new sqlite3.Database('src/databases/score.db');
 
 async function scoreCache() {
+    scoreMapCache.clear();
     db.all(`SELECT * FROM scores`, function (err, result) {
         if (err) throw err;
         for (i in result) scoreMapCache.set(result[i].id, result[i].points);
@@ -17,12 +18,12 @@ async function addScore(id, points = 1) {
     if (userscore == undefined) {
         db.run(`INSERT INTO scores(id, points) VALUES(?, ?)`, [id, points], function (err) {
             if (err) throw err;
-            scoreMapCache.set(id, 1);
+            scoreMapCache.set(id, points);
         });
     } else {
-        db.run(`UPDATE scores SET points = ? WHERE id = ?`, [userscore + points, id], function (err) {
+        db.run(`UPDATE scores SET points = ? WHERE id = ?`, [Number(userscore) + Number(points), id], function (err) {
             if (err) throw err;
-            scoreMapCache.set(id, userscore + 1);
+            scoreMapCache.set(id, Number(userscore) + Number(points));
         });
     }
 }
@@ -31,9 +32,9 @@ async function deleteScore(id, points) {
     const userscore = scoreMapCache.get(id);
     if (userscore == undefined) return false;
     else if (points) {
-        db.run(`UPDATE scores SET points = ? WHERE id = ?`, [userscore - points, id], function (err) {
+        db.run(`UPDATE scores SET points = ? WHERE id = ?`, [Number(userscore) - Number(points), id], function (err) {
             if (err) throw err;
-            scoreMapCache.set(id, userscore - points);
+            scoreMapCache.set(id, Number(userscore) - Number(points));
         })
     } else {
         db.run(`DELETE FROM scores WHERE id = ?`, [id], function (err) {
