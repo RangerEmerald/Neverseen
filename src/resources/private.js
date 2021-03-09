90479828fd5f1139a6db670ac36e1e08e7d0ab5e
Ranger Emerald <58153205+RangerEmerald@users.noreply.github.com>
2021-03-09 12:53:11 -0700

    Updated for welcome messages

--- src/resources/private.js
+++ src/resources/private.js
@@ -1,5 +1,5 @@
 let private = {
-    "randommessages": {"816446836784889907": "816450220530597918", "709195031822598255": "724461994853531659"},
+    "allowed": {"816446836784889907": {mainchat: "816450220530597918", welcome: {channel: "816446836784889910", welcomemessage: "hello <@![id]>, get your kazoo ready", leavemessage: "bye <@![id]>, we will remember you"}}, "709195031822598255": {mainchat: "724461994853531659", welcome: {channel: "772284955974828042", welcomemessage: "Welcome <@![id]> to the bot testing server!", leavemessage: "<@![id]> left"}}},
     "control": "817103853517930538"
 }
 
