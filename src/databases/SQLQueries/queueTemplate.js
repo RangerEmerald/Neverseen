449781ccd490b48793e9433177a6061872295e9f
Ranger Emerald <58153205+RangerEmerald@users.noreply.github.com>
2021-03-09 12:52:27 -0700

    Deleted music function from bot

--- src/databases/SQLQueries/queueTemplate.js
+++
@@ -1,9 +0,0 @@
-class Queue {
-    constructor (discordid, name, queue) {
-        this.discordid = discordid;
-        this.name = name;
-        this.queue = queue;
-    }
-}
-
-module.exports = { Queue };
