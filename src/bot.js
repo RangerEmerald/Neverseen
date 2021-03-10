745d294248028f555577be57941915aa15937188
Ranger Emerald <58153205+RangerEmerald@users.noreply.github.com>
2021-03-10 15:12:01 -0700

    Override kazoo keyword in spam channel
    If people use command "kazoo" in spam channel, and does not contribute to overall respond usage

--- src/bot.js
+++ src/bot.js
@@ -243,9 +243,9 @@ client.on('message', async message => {
     for (i in replylist) {
         let user = userReplyMessage.get(message.author.id);
         
-        if ((similarity.similarity(replylist[i][0], message.content.toLowerCase()) > 0.69 || similarity.similarity(replylist[i][0].replace(/[ ]/g, ''), message.content.toLowerCase().replace(/[ ]/g, '')) > 0.69 || message.content.toLowerCase().indexOf(replylist[i][0]) != -1) && (!user || user < 5)) {
-            if (user != undefined) userReplyMessage.set(message.author.id, (userReplyMessage.get(message.author.id))+1); 
-            else userReplyMessage.set(message.author.id, 1);
+        if ((similarity.similarity(replylist[i][0], message.content.toLowerCase()) > 0.69 || similarity.similarity(replylist[i][0].replace(/[ ]/g, ''), message.content.toLowerCase().replace(/[ ]/g, '')) > 0.69 || message.content.toLowerCase().indexOf(replylist[i][0]) != -1) && ((!user || user < 5) || message.channel.name.toLowerCase() == "spam")) {
+            if (user != undefined && message.channel.name.toLowerCase() != "spam") userReplyMessage.set(message.author.id, (userReplyMessage.get(message.author.id))+1); 
+            else if (message.channel.name.toLowerCase() != "spam") userReplyMessage.set(message.author.id, 1);
             
             if (similarity.similarity(replylist[i][0], message.content.toLowerCase()) > 0.69) message.channel.send(replylist[i][1]);
             else if (similarity.similarity(replylist[i][0].replace(/[ ]/g, ''), message.content.toLowerCase().replace(/[ ]/g, '')) > 0.69) message.channel.send(replylist[i][1]);
